ad3de217ee38f168a39db9a0f8e401f7b02a925b
vinay thallam <vinay.thallam@jifflenow.com>
2018-12-28 16:02:59 +0530

    react relay initial setup

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import LinkListPage from './LinkListPage'
+
+class App extends Component {
+  render() {
+    return <LinkListPage />
+  }
+}
+
+export default App;
