b445736f1457014a820aa81641c172747ed06ea6
vinay thallam <vinay.thallam@jifflenow.com>
2019-01-03 09:38:19 +0530

    add Authorization header to authenticate all API request after login

--- src/Environment.js
+++ src/Environment.js
@@ -1,3 +1,5 @@
+import { GC_AUTH_TOKEN } from './constants'
+
 // 1
 const {
     Environment,
@@ -16,7 +18,8 @@ const network = Network.create((operation, variables) => {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem(GC_AUTH_TOKEN)}`
         },
         body: JSON.stringify({
             query: operation.text,
